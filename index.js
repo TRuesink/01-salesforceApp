3603e831aa8643b02c4cecc5499f5e8e240baa11
Tim Ruesink <timothy.ruesink@gmail.com>
2020-12-29 13:23:39 -0600

    fixed index file

--- index.js
+++ index.js
@@ -44,6 +44,16 @@ app.use("/api/v1/metadata", metadataRoutes);
 // custom error handling middleware
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve production assets like our main.js file or main.css file
+  app.use(express.static("client/build"));
+  // Express will serve the index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
