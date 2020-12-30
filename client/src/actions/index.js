e1d3a1be28004f3d71b39ba1db0d4d2fb043b866
Tim Ruesink <timothy.ruesink@gmail.com>
2020-12-29 21:49:54 -0600

    added try catch blocks to action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -88,34 +88,49 @@ export const changeEditMode = (status) => {
 // Fetctch accounts
 export const fetchAccounts = (params) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_ACCOUNT });
-    const response = await axios.get(`/api/v1/sobjects/Account`, {
-      params: params,
-    });
-    dispatch({ type: FETCH_ACCOUNTS, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_ACCOUNT });
+      const response = await axios.get(`/api/v1/sobjects/Account`, {
+        params: params,
+      });
+      dispatch({ type: FETCH_ACCOUNTS, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // create account
 export const createAccount = (formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_ACCOUNT });
-    const response = await axios.post(`/api/v1/sobjects/Account`, formValues);
-    dispatch({ type: CREATE_ACCOUNT, payload: response.data });
-    history.push("/accounts");
+    try {
+      dispatch({ type: IN_PROGRESS_ACCOUNT });
+      const response = await axios.post(`/api/v1/sobjects/Account`, formValues);
+      dispatch({ type: CREATE_ACCOUNT, payload: response.data });
+      history.push("/accounts");
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // update an account
 export const updateAccount = (id, formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_ACCOUNT });
-    const response = await axios.put(
-      `/api/v1/sobjects/Account/${id}`,
-      formValues
-    );
-    dispatch({ type: UPDATE_ACCOUNT, payload: response.data });
-    dispatch({ type: VIEW });
+    try {
+      dispatch({ type: IN_PROGRESS_ACCOUNT });
+      const response = await axios.put(
+        `/api/v1/sobjects/Account/${id}`,
+        formValues
+      );
+      dispatch({ type: UPDATE_ACCOUNT, payload: response.data });
+      dispatch({ type: VIEW });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
@@ -131,7 +146,6 @@ export const fetchContacts = (params) => {
       dispatch({ type: FETCH_CONTACTS, payload: response.data });
       dispatch({ type: NO_ERROR });
     } catch (error) {
-      console.log(error.response);
       dispatch({ type: ERROR, payload: error.response });
     }
   };
@@ -140,23 +154,33 @@ export const fetchContacts = (params) => {
 // create contact
 export const createContact = (formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_CONTACT });
-    const response = await axios.post(`/api/v1/sobjects/Contact`, formValues);
-    dispatch({ type: CREATE_CONTACT, payload: response.data });
-    history.push("/contacts");
+    try {
+      dispatch({ type: IN_PROGRESS_CONTACT });
+      const response = await axios.post(`/api/v1/sobjects/Contact`, formValues);
+      dispatch({ type: CREATE_CONTACT, payload: response.data });
+      dispatch({ type: NO_ERROR });
+      history.push("/contacts");
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // update a contact
 export const updateContact = (id, formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_CONTACT });
-    const response = await axios.put(
-      `/api/v1/sobjects/Contact/${id}`,
-      formValues
-    );
-    dispatch({ type: UPDATE_CONTACT, payload: response.data });
-    dispatch({ type: VIEW });
+    try {
+      dispatch({ type: IN_PROGRESS_CONTACT });
+      const response = await axios.put(
+        `/api/v1/sobjects/Contact/${id}`,
+        formValues
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: response.data });
+      dispatch({ type: VIEW });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
@@ -164,37 +188,52 @@ export const updateContact = (id, formValues) => {
 // get opportunities
 export const fetchOpportunities = (params) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_OPPORTUNITY });
-    const response = await axios.get(`/api/v1/sobjects/Opportunity`, {
-      params: params,
-    });
-    dispatch({ type: FETCH_OPPORTUNITIES, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_OPPORTUNITY });
+      const response = await axios.get(`/api/v1/sobjects/Opportunity`, {
+        params: params,
+      });
+      dispatch({ type: FETCH_OPPORTUNITIES, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // create an opportunity
 export const createOpportunity = (formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_OPPORTUNITY });
-    const response = await axios.post(
-      `/api/v1/sobjects/Opportunity`,
-      formValues
-    );
-    dispatch({ type: CREATE_OPPORTUNITY, payload: response.data });
-    history.push("/opportunities");
+    try {
+      dispatch({ type: IN_PROGRESS_OPPORTUNITY });
+      const response = await axios.post(
+        `/api/v1/sobjects/Opportunity`,
+        formValues
+      );
+      dispatch({ type: CREATE_OPPORTUNITY, payload: response.data });
+      dispatch({ type: NO_ERROR });
+      history.push("/opportunities");
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // update an opportunity
 export const updateOpportunity = (id, formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_OPPORTUNITY });
-    const response = await axios.put(
-      `/api/v1/sobjects/Opportunity/${id}`,
-      formValues
-    );
-    dispatch({ type: UPDATE_OPPORTUNITY, payload: response.data });
-    dispatch({ type: VIEW });
+    try {
+      dispatch({ type: IN_PROGRESS_OPPORTUNITY });
+      const response = await axios.put(
+        `/api/v1/sobjects/Opportunity/${id}`,
+        formValues
+      );
+      dispatch({ type: UPDATE_OPPORTUNITY, payload: response.data });
+      dispatch({ type: VIEW });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
@@ -202,132 +241,198 @@ export const updateOpportunity = (id, formValues) => {
 // fetch tasks
 export const fetchTasks = (params) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_TASK });
-    const response = await axios.get(`/api/v1/sobjects/Task`, {
-      params: params,
-    });
-    dispatch({ type: FETCH_TASKS, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_TASK });
+      const response = await axios.get(`/api/v1/sobjects/Task`, {
+        params: params,
+      });
+      dispatch({ type: FETCH_TASKS, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 // update task
 export const updateTask = (id, formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_TASK });
-    const response = await axios.put(`/api/v1/sobjects/Task/${id}`, formValues);
-    dispatch({ type: UPDATE_TASK, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_TASK });
+      const response = await axios.put(
+        `/api/v1/sobjects/Task/${id}`,
+        formValues
+      );
+      dispatch({ type: UPDATE_TASK, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 //create task
 export const createTask = (type, formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_TASK });
-    const response = await axios.post(`/api/v1/sobjects/Task`, formValues);
-    dispatch({ type: CREATE_TASK, payload: response.data });
-    history.push(`/${type}/${formValues.WhatId || formValues.WhoId}/tasks`);
+    try {
+      dispatch({ type: IN_PROGRESS_TASK });
+      const response = await axios.post(`/api/v1/sobjects/Task`, formValues);
+      dispatch({ type: CREATE_TASK, payload: response.data });
+      dispatch({ type: NO_ERROR });
+      history.push(`/${type}/${formValues.WhatId || formValues.WhoId}/tasks`);
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // metadata actions
 export const fetchMetadata = (type) => {
   return async (dispatch) => {
-    const response = await axios.get(`/api/v1/sobjects/metadata/${type}`);
-    dispatch({ type: FETCH_METADATA, payload: response.data.data });
+    try {
+      const response = await axios.get(`/api/v1/sobjects/metadata/${type}`);
+      dispatch({ type: FETCH_METADATA, payload: response.data.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // fetch paths
 export const fetchPaths = () => {
   return async (dispatch) => {
-    const response = await axios.get(`/api/v1/metadata/PathAssistant`);
-    dispatch({ type: FETCH_PATHS, payload: response.data.data });
-  };
-};
-
-// CHATTER ACTIONS
-// Fetch groups
-export const fetchGroups = () => {
-  return async (dispatch) => {
     try {
-      dispatch({ type: IN_PROGRESS_GROUPS });
-      const response = await axios.get(`/api/v1/chatter/groups`);
-      dispatch({ type: FETCH_GROUPS, payload: response.data });
+      const response = await axios.get(`/api/v1/metadata/PathAssistant`);
+      dispatch({ type: FETCH_PATHS, payload: response.data.data });
+      dispatch({ type: NO_ERROR });
     } catch (error) {
-      console.log(error.response);
       dispatch({ type: ERROR, payload: error.response });
     }
   };
 };
 
+// CHATTER ACTIONS
+// Fetch groups
+// export const fetchGroups = () => {
+//   return async (dispatch) => {
+//     try {
+//       dispatch({ type: IN_PROGRESS_GROUPS });
+//       const response = await axios.get(`/api/v1/chatter/groups`);
+//       dispatch({ type: FETCH_GROUPS, payload: response.data });
+//     } catch (error) {
+//       console.log(error.response);
+//       dispatch({ type: ERROR, payload: error.response });
+//     }
+//   };
+// };
+
 // LEAD ACTIONS
 // get all leads
 export const fetchLeads = (params) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_LEAD });
-    const response = await axios.get(`/api/v1/sobjects/Lead`, {
-      params: params,
-    });
-    dispatch({ type: FETCH_LEADS, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_LEAD });
+      const response = await axios.get(`/api/v1/sobjects/Lead`, {
+        params: params,
+      });
+      dispatch({ type: FETCH_LEADS, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // update a lead
 export const updateLead = (id, formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_LEAD });
-    const response = await axios.put(`/api/v1/sobjects/Lead/${id}`, formValues);
-    dispatch({ type: UPDATE_LEAD, payload: response.data });
-    dispatch({ type: VIEW });
+    try {
+      dispatch({ type: IN_PROGRESS_LEAD });
+      const response = await axios.put(
+        `/api/v1/sobjects/Lead/${id}`,
+        formValues
+      );
+      dispatch({ type: UPDATE_LEAD, payload: response.data });
+      dispatch({ type: VIEW });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 // create a lead
 export const createLead = (formValues) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_LEAD });
-    const response = await axios.post(`/api/v1/sobjects/Lead`, formValues);
-    dispatch({ type: CREATE_LEAD, payload: response.data });
-    history.push("/leads");
+    try {
+      dispatch({ type: IN_PROGRESS_LEAD });
+      const response = await axios.post(`/api/v1/sobjects/Lead`, formValues);
+      dispatch({ type: CREATE_LEAD, payload: response.data });
+      dispatch({ type: NO_ERROR });
+      history.push("/leads");
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 //search action
 export const searchOpportunities = (type, term, fields) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_OPPORTUNITY });
-    const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
-      params: { term: term, fields: fields },
-    });
-    dispatch({ type: OPPORTUNITY_SEARCH, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_OPPORTUNITY });
+      const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
+        params: { term: term, fields: fields },
+      });
+      dispatch({ type: OPPORTUNITY_SEARCH, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 export const searchLeads = (type, term, fields) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_LEAD });
-    const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
-      params: { term: term, fields: fields },
-    });
-    dispatch({ type: LEAD_SEARCH, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_LEAD });
+      const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
+        params: { term: term, fields: fields },
+      });
+      dispatch({ type: LEAD_SEARCH, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 export const searchAccounts = (type, term, fields) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_ACCOUNT });
-    const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
-      params: { term: term, fields: fields },
-    });
-    dispatch({ type: ACCOUNT_SEARCH, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_ACCOUNT });
+      const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
+        params: { term: term, fields: fields },
+      });
+      dispatch({ type: ACCOUNT_SEARCH, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
 
 export const searchContacts = (type, term, fields) => {
   return async (dispatch) => {
-    dispatch({ type: IN_PROGRESS_CONTACT });
-    const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
-      params: { term: term, fields: fields },
-    });
-    dispatch({ type: CONTACT_SEARCH, payload: response.data });
+    try {
+      dispatch({ type: IN_PROGRESS_CONTACT });
+      const response = await axios.get(`/api/v1/sobjects/search/${type}`, {
+        params: { term: term, fields: fields },
+      });
+      dispatch({ type: CONTACT_SEARCH, payload: response.data });
+      dispatch({ type: NO_ERROR });
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response });
+    }
   };
 };
